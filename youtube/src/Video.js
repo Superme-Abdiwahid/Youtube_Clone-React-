ce8da0157100ccaf5b62425514945fd0264e7f44
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-03-25 00:11:27 -0700

    Update client side

--- youtube/src/Video.js
+++ youtube/src/Video.js
@@ -6,4 +6,5 @@ export default class Video extends React.Component{
         <iframe width="560" height="315" src="https://www.youtube.com/embed/HrYtwz0Xe2Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       )
     }
+
   }
